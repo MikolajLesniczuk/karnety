88f5806b7903aff3ced2cdfe67b5f6ced6a6450a
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2024-06-20 22:09:00 +0200

    emes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import SeniorGroup from "./components/SeniorGroup";
 import JuniorGroup from "./components/JuniorGroup";
+import WelcomePage from "./components/WelcomePage";
+import { AuthProvider, useAuth } from "../src/components/AuthContext";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/junior">Grupa Młodsza</Link>
-              </li>
-              <li>
-                <Link to="/senior">Grupa Starsza</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/senior" element={<SeniorGroup />} />
-            <Route path="/junior" element={<JuniorGroup />} />
-            <Route path="/" element={<SeniorGroup />} />
-          </Routes>
-        </header>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+              <Route path="/*" element={<ProtectedRoutes />} />
+            </Routes>
+          </header>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
+
+const ProtectedRoutes = () => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Routes>
+      <Route path="senior" element={<SeniorGroup />} />
+      <Route path="junior" element={<JuniorGroup />} />
+      <Route path="/" element={<Navigate to="senior" />} />
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/components/WelcomePage.js
@@ -0,0 +1,39 @@
+// WelcomePage.js
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./WelcomePage.css";
+import { useAuth } from "./AuthContext";
+
+const WelcomePage = () => {
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (login(password)) {
+      navigate("/senior");
+    } else {
+      setError("Niepoprawne hasło");
+    }
+  };
+
+  return (
+    <div className="welcome-page">
+      <h1>Witaj! Podaj hasło</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Hasło"
+        />
+        <button type="submit">Wejdź</button>
+      </form>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
+
+export default WelcomePage;
